6769fe15728e3c7033e43b63aa48397dcc9ee63b
iwaitu <iwaitu@aliyun.com>
2023-10-01 13:46:23 +0800

    Update start-jupyter.sh

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -4,4 +4,4 @@
 source /opt/conda/bin/activate
 
 # 启动 Jupyter Lab
-jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --notebook-dir=/root/workspace
+jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --notebook-dir=/root/workspace --ServerApp.iopub_msg_rate_limit=1000000000
